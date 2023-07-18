3bda3af005583ddcd82a31c6203bc74fa94083dc
Fluffy1908 <pro1shinov.tima@gmail.com>
2023-07-18 16:33:54 -0400

    added header info 1

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div>
+            <header className="header-elem">
+                <img src="/images/recyclingLogo.png"></img>
+                <p>Home</p>
+                <p>About Us</p>
+                <p>What We Do</p>
+            </header>
+        </div>
+    )
+}
