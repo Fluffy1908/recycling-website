a94c5a7b15d9f47d2b033c7ab42bf6f7f3de0b71
Fluffy1908 <pro1shinov.tima@gmail.com>
2023-07-18 21:40:56 -0400

    some changes to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,9 +5,17 @@ export default function Header() {
         <div>
             <header className="header-elem">
                 <img src="/images/recyclingLogo.png"></img>
-                <p>Home</p>
-                <p>About Us</p>
-                <p>What We Do</p>
+                    <p>Home</p>
+                    <p>About Us</p>
+                    <p>What We Do</p>
+                    <p>Our Products</p>
+                    <p>Applicaitons</p>
+                    <p>News</p>
+                    <p>Contact Us</p>
+                <div className="language-options">
+                    <p className="eng-option">ENG</p>
+                    <p className="ukr-option">UKR</p>
+                </div>
             </header>
         </div>
     )
