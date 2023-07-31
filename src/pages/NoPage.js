028e39193d3b5ae2ee35a99f99b0b0c5b8cf5678
Fluffy1908 <pro1shinov.tima@gmail.com>
2023-07-31 14:09:40 -0400

    added nav functionality to site, next gonna work on multi language with new library, also gonna create new branch called language-func

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 export default function NoPage () {
     return (
         <>
+            <Navbar />
             <h1>Error 404: Page not found</h1>
         </>
     )
