a07ea9bf2cc3a39b8bfd1dedceb7710e3696061f
Fluffy1908 <pro1shinov.tima@gmail.com>
2023-08-21 00:31:09 -0400

    added website responsive basic logic, needs improvement tomorrow.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import './Navbar.css'
+import './NavbarStyles.css'
 import { useTranslation } from 'react-i18next'
+
 
 function Navbar() {
         const [t, i18n] = useTranslation("global");
+        const [click, setClick] = useState(false);
+
         const logo = "/images/recyclingLogo.png"
-        
-        const toggleButton = document.getElementsByClassName('toggle-button')[0]
-        const navbarLinks = document.getElementsByClassName('navbar-links')[0]
         
         const handleChangeLanguage = (lang) => {
             i18n.changeLanguage(lang)
@@ -16,60 +16,62 @@ function Navbar() {
 
     return (
         <>
-            <nav className='navbar'>
-                <Link to='/' className='navbar-logo'>
-                    <div className='brand-image'><img src={logo}></img></div>
+            <nav>
+                <Link to='/'> 
+                    <img src={logo} />
                 </Link>
-                <Link to='/' className='toggle-button' onClick={() => {
-                     navbarLinks.classList.toggle('active')
-                }}>
-                    <span className='bar'></span>
-                    <span className='bar'></span>
-                    <span className='bar'></span>
-                </Link>
-                <div className='navbar-links'>
-                    <ul>
+                <div>
+                    <ul id="navbar">
                         <li className='nav-item'>
-                            <Link to='/' className='nav-links' style={{textDecoration: 'none'}}>
-                                <span>{t("navbar.home")}</span>
+                            <Link to='/'>
+                                <span className="active">{t("navbar.home")}</span>
                             </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/about-us' className='nav-links' style={{textDecoration: 'none'}}>
+                            <Link to='/about-us' >
                                 <span>{t("navbar.aboutUs")}</span>
                             </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/what-we-do' className='nav-links' style={{textDecoration: 'none'}}>
+                            <Link to='/what-we-do' >
                                 <span>{t("navbar.whatWeDo")}</span>
                             </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/our-products' className='nav-links-mobile' style={{textDecoration: 'none'}}>
+                            <Link to='/our-products'>
                                 <span>{t("navbar.ourProducts")}</span>
                             </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/applications' className='nav-links-mobile' style={{textDecoration: 'none'}}>
+                            <Link to='/applications'>
                                 <span>{t("navbar.applications")}</span>
                             </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/news' className='nav-links-mobile' style={{textDecoration: 'none'}}>
+                            <Link to='/news' >
                                 <span>{t("navbar.news")}</span>
                             </Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/contact-us' className='nav-links-mobile' style={{textDecoration: 'none'}}>
-                            <span>{t("navbar.contactUs")}</span>
+                            <Link to='/contact-us' >
+                                <span>{t("navbar.contactUs")}</span>
                             </Link>
                         </li>
                     </ul>
                 </div>
-                <div className='language-options'>
+                {/* <div className='language-options'>
                     <span className="individual-lang-option-en" onClick={() => handleChangeLanguage("en")}>ENG</span>
                     <span className="individual-lang-option-ru" onClick={() => handleChangeLanguage("ru")}>RU</span>
                     <span className="individual-lang-option-ua" onClick={() => handleChangeLanguage("ua")}>UA</span>
+                </div> */}
+
+                <div id='mobile'
+                onClick={() => setClick(!click)}>
+                    <i 
+                    id='bar'
+                    className=
+                    {click ? "fas fa-times" : "fas fa-bars"}
+                    ></i>
                 </div>
             </nav>
         </>
