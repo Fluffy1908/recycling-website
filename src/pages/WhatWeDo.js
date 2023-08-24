867ba52d7893d163fef377c26b35894dcfe217a9
Fluffy1908 <pro1shinov.tima@gmail.com>
2023-08-23 23:47:01 -0400

    added updates on what we do page

--- src/pages/WhatWeDo.js
+++ src/pages/WhatWeDo.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import './WhatWeDo.css';
+
 
 export default function WhatWeDo () {
+    const starIcon = "/images/star.png"
+
     return (
         <>
             <Navbar />
-            <h1>What We Do Page</h1>
+                <div className="what-we-do-text">
+                    <h2>What We Do</h2>
+                    <img src={starIcon}></img>
+                    <p>Our company is engaged in the acceptance of used tires from companies and individuals in any quantity. Our recyclers benefit from used tires by crushing them into small granules, which can then be used to make rubber products, rubber floors, rubberized asphalt, playground mulch and more, click here to view the application. Tire collection points will soon appear in all corners of Ukraine to ensure the delivery of tires by enterprises and the population. Click here to view tire pick-up points. Recycling points will also be updated to ensure that the required volume of tires is recycled throughout Ukraine. To view the tire recycling points click here .</p>
+                </div>
             <Footer />
         </>
     ) 
