e71676dd6c68328809bd4f200537cd502841e0b6
Fluffy1908 <pro1shinov.tima@gmail.com>
2023-09-18 15:47:45 -0400

    added eng-json-i18next

--- src/components/main.js
+++ src/components/main.js
@@ -28,22 +28,22 @@ export default function Main() {
             </div>
 
             <div className="dispose-tires mt-4">
-                <h2 className="dispose-tires-text">Dispose Tires</h2>
-                <p className="mt-3">The main priority of main-our company is the most convenient return of used tires for enterprises and population. If you have worn tires, please click on the button below to fill out a short form and main-our specialist will contact you. Some text to test here</p>
-                <button className="dispose-button mt-3">Click me!</button>
+                <h2 className="dispose-tires-text">{t("dispose-tires-main.dispose-tires-text-upper")}</h2>
+                <p className="mt-3">{t("dispose-tires-main.dispose-tires-text")}</p>
+                <button className="dispose-button mt-3">{t("dispose-tires-main.button-text")}</button>
             </div>
 
             <div className="product-section mt-4">
-                <h2>Our production</h2>
+                <h2>{t("production-section-main.our-production-text-upper")}</h2>
                 <img src="/images/gold-ingots.png" alt="Products" className="gold-ingots mt-3"/>
-                <p className="mt-4">Suspendisse sed interdum purus. Maecenas mattis nunc ut nunc bibendum venenatis. Nulla risus tortor, eleifend quis ipsum at, blandit accumsan dolor. Cras eu ultricies urna, quis viverra tortor. Nulla euismod lobortis</p>
+                <p className="mt-4">{t("production-section-main.our-production-text")}</p>
             </div>
         
         {/* Our Products section start */}
             <div className="main-our-product-gallery">
                 <div className="main-our-product-image-container">
                     <img src={testImage} alt="Image" />
-                    <div className="product-centered"><h2>Hello</h2></div>
+                    <div className="product-centered"><h2>{t("production-gallery-main.crumb-rubber-photo-text")}</h2></div>
                     <div className="product-lower-center"><p>Hello</p></div>
                 </div>
 
@@ -81,9 +81,9 @@ export default function Main() {
             </div>
 
             <div className="product-section mt-4">
-                <h2>Applications</h2>
+                <h2>{t("application-section-main.application-text-upper")}</h2>
                 <img src="/images/gold-ingots.png" alt="Products" className="gold-ingots mt-3"/>
-                <p className="mt-4">Suspendisse sed interdum purus. Maecenas mattis nunc ut nunc bibendum venenatis. Nulla risus tortor, eleifend quis ipsum at, blandit accumsan dolor. Cras eu ultricies urna, quis viverra tortor. Nulla euismod lobortis leo sit amet euismod. Nullam elementum maximus mi. In tristique malesuada enim quis sollicitudin. Morbi condimentum elementum ipsum, sed eleifend neque molestie id. Nullam rhoncus egestas lectus, sed eleifend risus venenatis quis. Vestibulum egestas arcu vel sem maximus dignissim.</p>
+                <p className="mt-4">{t("application-section-main.application-text")}</p>
             </div>
             
             {/* Application gallery start */}
