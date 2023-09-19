24df1575b6832a0e8778d357761b1f43840dfd72
Fluffy1908 <pro1shinov.tima@gmail.com>
2023-09-18 22:15:55 -0400

    added func and language options

--- src/index.js
+++ src/index.js
@@ -29,10 +29,9 @@ i18n
         global: global_ua
       }
     },
-    lng: "en",
-    fallbackLng: "en",
+    fallbackLng: "ru", //In case other languages fail
     detection: {
-      order: ['htmlTag', 'cookie'],
+      order: ['cookie', 'htmlTag', 'path', 'localStorage', 'subdomain'], //Using language detection
       caches: ['cookie'],
     }
   });
@@ -49,4 +48,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
